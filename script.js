966ee66f188427b9ae1d0beac5979548fc076cea
verbytsky <j.wierzbicki1997@gmail.com>
2023-06-01 23:55:14 +0300

    Implemented a redesign of the portfolio website,

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-window.addEventListener('resize', function() {
-    if(window.innerWidth < 600) {
-        document.body.classList.add('mobile');
-    } else {
-        document.body.classList.remove('mobile');
-    }
+document.addEventListener('mousemove', function(e) {
+  let x = e.clientX / window.innerWidth;
+  let y = e.clientY / window.innerHeight;  
+  document.documentElement.style.setProperty('--x', x);
+  document.documentElement.style.setProperty('--y', y);
 });
+
+
+
+document.getElementById('profile-link').addEventListener('click', function(e) {
+  e.preventDefault();
+  this.classList.add('no-glow');
+  document.getElementById('modal').style.display = 'block';
+});
+
+window.onclick = function(event) {
+  if (event.target == document.getElementById('modal')) {
+    document.getElementById('modal').style.display = 'none';
+    document.getElementById('profile-link').classList.remove('no-glow');
+  }
+}
+
+
